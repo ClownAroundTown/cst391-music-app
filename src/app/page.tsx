5bf1a5377082d0dc94b95eacbd2eca0928e756e1
ClownAroundTown <sstorres270@gmail.com>
2026-04-05 21:51:29 -0700

    have you ever had a dream

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,12 @@ import styles from "./page.module.css";
 import {allAlbums} from "@/lib/db";
 import Card from "@/lib/Card/Card";
 import NavBar from "@/lib/NavBar/NavBar";
-// Source - https://stackoverflow.com/a/44985246
-// Posted by Praveen M P, modified by community. See post 'Timeline' for change history
-// Retrieved 2026-04-05, License - CC BY-SA 4.0
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SearchForm from "@/lib/SearchForm/SearchForm";
+import { connection } from 'next/server';
 
 async function Page(){
+  await connection()
   const albums = await allAlbums()
   const deck = albums.map((AlbumOBJ) => {
      const handleSelectionOne = (albumId:number) => {
